fbb27831c7c176b36bc31c73dd72fce39c0f11d5
FGARCIA\Usuario <fgarcia@cidenet.com.co>
2021-04-25 21:13:30 -0500

    feat: init commit

---
+++ src/main/java/com/wchallange/jsonplaceholder/JsonplaceholderApplication.java
@@ -0,0 +1,17 @@
+package com.wchallange.jsonplaceholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class JsonplaceholderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonplaceholderApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/wchallange/jsonplaceholder/JsonplaceholderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wchallange.jsonplaceholder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonplaceholderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
