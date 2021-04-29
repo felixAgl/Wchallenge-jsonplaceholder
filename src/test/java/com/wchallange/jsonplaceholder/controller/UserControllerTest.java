3b283759b708d0d149a992271f503c1f7cd6d911
FGARCIA\Usuario <fgarcia@cidenet.com.co>
2021-04-28 21:55:36 -0500

    test: add test controller

---
+++ src/test/java/com/wchallange/jsonplaceholder/controller/UserControllerTest.java
@@ -0,0 +1,43 @@
+package com.wchallange.jsonplaceholder.controller;
+
+import com.wchallange.jsonplaceholder.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ContextConfiguration(classes = {UserController.class})
+@ExtendWith(SpringExtension.class)
+public class UserControllerTest {
+    @Autowired
+    private UserController userController;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void testConsumeUser() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/users/{id}", 1);
+        MockMvcBuilders.standaloneSetup(this.userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void testConsumeUser2() throws Exception {
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/users/{id}", 1);
+        getResult.contentType("text/plain");
+        MockMvcBuilders.standaloneSetup(this.userController)
+                .build()
+                .perform(getResult)
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
+
